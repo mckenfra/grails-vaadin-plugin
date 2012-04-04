547af21115e987d073dd9af4cd636b9d53746c9e
Francis McKenzie <mail@hkwebentrepreneurs.com>
2012-04-04 02:30:46 +0200

    Added package javadocs

---
+++ source-code/src/java/com/vaadin/grails/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Internal classes used by Vaadin plugin, e.g. for autowiring
+ */
+package com.vaadin.grails;
---
+++ source-code/src/java/com/vaadin/grails/terminal/gwt/server/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Internal servlet classes used by Vaadin plugin. 
+ */
+package com.vaadin.grails.terminal.gwt.server;
