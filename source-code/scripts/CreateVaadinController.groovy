bb7efcd1e2e246c7eef1fdb9c060b3297c1e6c7f
Francis McKenzie <mail@hkwebentrepreneurs.com>
2012-04-14 18:42:16 +0200

    Should create Vaadin controllers in grails-app/controllers

--- source-code/scripts/CreateVaadinController.groovy
+++ source-code/scripts/CreateVaadinController.groovy
@@ -15,7 +15,7 @@ target ('default': "Creates a new Vaadin controller") {
 
     for (name in argsMap["params"]) {
         name = purgeRedundantArtifactSuffix(name, type)
-        createArtifact(name: name, suffix: type, type: type, path: "grails-app/vaadin", templatePath: "templates/vaadin/artifacts")
+        createArtifact(name: name, suffix: type, type: type, path: "grails-app/controllers", templatePath: "templates/vaadin/artifacts")
 
         createUnitTest(name: name, suffix: type, superClass: "ControllerUnitTestCase")
     }
