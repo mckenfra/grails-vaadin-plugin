ab88dbced1eb1585288397b84a9416211e496c8a
Francis McKenzie <mail@hkwebentrepreneurs.com>
2012-06-10 23:12:06 +0800

    Comment change

--- source-code/grails-app/controllers/org/grails/plugin/vaadin/HomeVaadinController.groovy
+++ source-code/grails-app/controllers/org/grails/plugin/vaadin/HomeVaadinController.groovy
@@ -3,13 +3,16 @@ package org.grails.plugin.vaadin
 /**
  * Controller for displaying default application home page.
  * <p>
- * There should be no need to create a controller with the same
- * name in a project, as this class always simply displays the
- * "index" view.  
+ * End users can create a controller with the same name in a project
+ * if, for example, they want to redirect to another controller
+ * as the home page.
  * 
  * @author Francis McKenzie
  */
 class HomeVaadinController {
+    /**
+     * Renders the default home page <code>/vaadin/index</code>
+     */
     def index() {
         render view:"/vaadin/index"
     }
