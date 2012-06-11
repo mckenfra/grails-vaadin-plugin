f49caf0930ea0c7686cec3db36715bf73f6f804f
Francis McKenzie <mail@hkwebentrepreneurs.com>
2012-06-11 22:38:05 +0800

    Bugfix: support external links in user guide (grails doc doesn't support nested closures in config)

--- source-code/grails-app/conf/Config.groovy
+++ source-code/grails-app/conf/Config.groovy
@@ -1,9 +1,6 @@
-grails {
-    doc {
-        api.com.vaadin = "https://vaadin.com/api"
-        api.javax.servlet = "http://docs.oracle.com/javaee/6/api"
-        api.java.lang = "http://docs.oracle.com/javase/6/docs/api"
-        api.java.util = "http://docs.oracle.com/javase/6/docs/api"
-        api.groovy = "http://groovy.codehaus.org/gapi"
-    }
-}
+grails.doc.api.com.vaadin = "https://vaadin.com/api"
+grails.doc.api.javax.servlet = "http://docs.oracle.com/javaee/6/api"
+grails.doc.api.java.lang = "http://docs.oracle.com/javase/6/docs/api"
+grails.doc.api.java.util = "http://docs.oracle.com/javase/6/docs/api"
+grails.doc.api.groovy = "http://groovy.codehaus.org/gapi"
+
